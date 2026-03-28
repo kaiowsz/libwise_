2c9bb02c303eb7d969c1ca7d24651f082ace7fb3
kaiowsz <kaionpsa@gmail.com>
2026-03-28 10:47:18 -0300

    feat: Add experimental nextjs configurations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb'
+    }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "pub-*.r2.dev", // Prepare for Cloudflare R2
+      }
+    ],
+  },
 };
 
 export default nextConfig;
